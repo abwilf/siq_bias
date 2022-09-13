cf6519271dff006d7cd0c2a06a8dcc5c0326eb57
martinmamql <martin.ma.mql@gmail.com>
2022-09-13 16:45:52 -0400

    latest Social-IQ 1.0 re-annotation

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Use randomly initialized roberta
-python train_roberta_multiple_choice.py --dataset_path socialiq_taylor_new_qa_all_2022_08_processed --dataset_name socialiq_permute_a2 --output_dir output/socialiq_taylor_new_qa_all_2022_08_lr_1e-6_bs_3_unfreeze --learning_rate 1e-6 --batch_size 3
+CUDA_VISIBLE_DEVICES=0 python train_roberta_multiple_choice.py --dataset_path socialiq_taylor_2022_09_1.0_complete_processed --dataset_name socialiq_permute_a2 --output_dir output/socialiq_taylor_2022_09_1.0_complete_lr_1e-6_bs_3_unfreeze --learning_rate 1e-6 --batch_size 3
 # Use pretrained roberta
-python train_roberta_multiple_choice.py --dataset_path socialiq_taylor_new_qa_all_2022_08_processed --dataset_name socialiq_permute_a2 --output_dir output/socialiq_taylor_new_qa_all_2022_08_lr_1e-6_bs_3_unfreeze_pretrained --load_pretrained_lm --learning_rate 1e-6 --batch_size 3
+CUDA_VISIBLE_DEVICES=0 python train_roberta_multiple_choice.py --dataset_path socialiq_taylor_2022_09_1.0_complete_processed --dataset_name socialiq_permute_a2 --output_dir output/socialiq_taylor_2022_09_1.0_complete_processed_lr_1e-6_bs_3_unfreeze_pretrained --load_pretrained_lm --learning_rate 1e-6 --batch_size 3
